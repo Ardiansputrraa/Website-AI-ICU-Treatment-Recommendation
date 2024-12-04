97df2c3efa741b6616a09bc2008f5f324eb60d57
Ardiansputrraa <ardiansaputra2580@gmail.com>
2024-12-04 20:58:08 +0700

    update

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, render_template, current_app, Blueprint, jsonify, redirect, url_for
-import json
+import pandas as pd
 import jwt
 
-similarity_ = Blueprint('similarity', __name__)
+prediction_ = Blueprint('prediction', __name__)
 
-@similarity_.route('/similarity/<bed_id>')
-def summary(bed_id):
+@prediction_.route('/prediction/<bed_id>')
+def prediction(bed_id):
     myToken = request.cookies.get("mytoken")
     SECRET_KEY = current_app.config['SECRET_KEY']
     try:
         payload = jwt.decode(myToken, SECRET_KEY, algorithms=["HS256"])
         user_info = current_app.db.users.find_one({"email": payload["id"]})
-        return render_template('main/similarity.html', user_info=user_info, bed_id=bed_id)
+        return render_template('main/prediction.html', user_info=user_info, bed_id=bed_id)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("auth.sign_in", msg="Login time has expired!"))
     except jwt.exceptions.DecodeError:
