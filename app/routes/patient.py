957a0d12cd1b40ff885c59dccb4a37bddae7db54
Ardiansputrraa <ardiansaputra2580@gmail.com>
2024-12-09 00:31:15 +0700

    update source code

--- app/routes/patient.py
+++ app/routes/patient.py
@@ -1,6 +1,7 @@
 from flask import jsonify, Blueprint
 from app.services.vital_service import get_patient_detail, get_vital_data, get_blood_data
 from app.services.sofa_service import get_sofa_data
+from app.services.treatment_service import get_treatment_recommendation
 
 patient_ = Blueprint('patient', __name__)
 
@@ -22,4 +23,9 @@ def get_blood_data_by_bed(bed_id):
 @patient_.route('/get-sofa-data/<bed_id>', methods=['GET'])
 def get_sofa_data_by_bed(bed_id):
     result = get_sofa_data(bed_id)
+    return jsonify(result)
+
+@patient_.route('/get-treatment-data/<bed_id>', methods=['GET'])
+def get_treatment_data_by_bed(bed_id):
+    result = get_treatment_recommendation(bed_id)
     return jsonify(result)
