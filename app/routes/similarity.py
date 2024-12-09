8a9d3af7fb520b674a6f1c4e1fd03ac54b553a7c
Ardiansputrraa <ardiansaputra2580@gmail.com>
2024-12-10 02:16:45 +0700

    frontend backend similarity

--- app/routes/similarity.py
+++ app/routes/similarity.py
@@ -5,7 +5,7 @@ import jwt
 similarity_ = Blueprint('similarity', __name__)
 
 @similarity_.route('/similarity/<bed_id>')
-def summary(bed_id):
+def similarity(bed_id):
     myToken = request.cookies.get("mytoken")
     SECRET_KEY = current_app.config['SECRET_KEY']
     try:
@@ -16,3 +16,32 @@ def summary(bed_id):
         return redirect(url_for("auth.sign_in", msg="Login time has expired!"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("auth.sign_in", msg="Please login first!"))
+    
+@similarity_.route('/get-data-similarity', methods=['GET'])
+def get_data_similarity():
+    result = list(current_app.db.similarity_patient.find({}, {'_id' : False}))      
+    return jsonify(result)
+
+@similarity_.route('/search-data-similarity', methods=['POST'])
+def search_data_similarity():
+    filters = request.form.to_dict()  
+    query = {}
+
+    if filters.get("age"):
+        query["age"] = filters["age"]
+
+    if filters.get("gender"):
+        query["gender"] = filters["gender"]  
+
+    if filters.get("weight"):
+        query["weight"] = filters["weight"] 
+
+    if filters.get("height"):
+        query["height"] = filters["height"]  
+
+    try:
+        result = list(current_app.db.similarity_patient.find(query, {"_id": False}))
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": f"Error querying database: {str(e)}"}), 500
+
