e46208995a43ba69684f6c6987ed1aeb05109c04
Ardiansputrraa <ardiansaputra2580@gmail.com>
2025-01-25 18:39:37 +0700

    update

--- start.sh
+++ start.sh
@@ -1,22 +1,28 @@
+#!/bin/bash
 set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=.data/venv
+VIRTUALENV=".data/venv"
 
+# Memastikan virtual environment ada
 if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
+# Install pip jika belum ada
 if [ ! -f $VIRTUALENV/bin/pip ]; then
   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
-$VIRTUALENV/bin/pip install -r requirements.txt
+# Install dependencies
+$VIRTUALENV/bin/pip install --no-cache-dir -r requirements.txt
 
+# Build Tailwind CSS jika ada
 if [ -f tailwind.config.js ]; then
   echo "Building Tailwind CSS..."
-  npx tailwindcss -i ./src/input.css -o ./static/output.css --watch &
+  npx tailwindcss -i ./app/static/src/css/input.css -o ./app/static/dist/css/output.css --watch &
 fi
 
-$VIRTUALENV/bin/python3 app.py
+# Jalankan aplikasi dengan python di virtual environment
+$VIRTUALENV/bin/python app.py
