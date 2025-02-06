07d42f9ecd33039c5885f1ab0433ff31a6b1dfe6
Ardiansputrraa <ardiansaputra2580@gmail.com>
2025-02-06 08:24:27 +0700

    update

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from app import create_app
 from app.extensions import socketio
+import os
 
 app = create_app()
 
-if __name__ != "__main__":
-    socketio.init_app(app)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
